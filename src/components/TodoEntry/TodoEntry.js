b43869e81fb5cac71a4749f02d4b3a6f59fdd57e
Daniel Ahl <daniel.ahl@studentmedia.se>
2019-10-25 22:52:15 +0200

    Added cosmetics

--- src/components/TodoEntry/TodoEntry.js
+++ src/components/TodoEntry/TodoEntry.js
@@ -3,13 +3,14 @@ import './TodoEntry.css';
 
 const TodoEntry = (props) => {
     const [complete, setComplete] = useState(props.completed);
-    const [content] = useState(props.content);
+    const [content, setContent] = useState(props.content);
+    const [editMode, setEditMode] = useState(false);
     return (
         <div className={complete ? 'todo-entry completed' : 'todo-entry'}>
             <i className={complete ? 'far fa-check-square' : 'far fa-square'} onClick={() => setComplete(!complete)}></i>
-            <p>{content}</p>
+            {editMode ? <input type="text" onKeyUp={(event) => console.log(event)} />: <p>{content}</p>}
             <div className="todo-utils">
-                {complete ? '' : <i className="fas fa-pen"></i>}
+                {complete ? '' : <i className="fas fa-pen" onClick={() => setEditMode(true)}></i>}
                 <i className="far fa-trash-alt"></i>
             </div>
         </div>
